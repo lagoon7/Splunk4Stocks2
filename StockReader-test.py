b7ae97d665c4837c0d7f030baae42499df86fddf
lagoon7 <lag@trinity.local>
2014-04-28 22:37:07 -0400

    Added examples of argparse
    Added examples of argparsing.

--- StockReader-test.py
+++ StockReader-test.py
@@ -5,7 +5,31 @@ import argparse
 
 stocklist = list()
 stockline = list()
-fo = open(sys.argv[1],'r')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-if","--inputfile",help="Input File")
+parser.add_argument("-sd","--startdate", type=int,nargs=3,help="Start Date: Year[YYYY] Month[MM] Day [DD]")
+
+args = parser.parse_args()
+
+infilename = args.inputfile
+startyear= args.startdate[0]
+startmonth = args.startdate[1]
+startday = args.startdate[2]
+
+
+print "Year: ", startyear
+print "Month: ", startmonth
+print "Day: ", startday
+
+stockStartDate = datetime.datetime(startyear,startmonth,startday)
+
+print stockStartDate
+'''
+
+
+fo = open(infilename,'r')
 #Cheat Read first line
 header = fo.readline()
 for line in fo:
@@ -16,3 +40,4 @@ for line in fo:
 print "Number of items in list: %s",len(stocklist)
 print "100th item in list: %s", stocklist[100]
 fo.close()
+'''
